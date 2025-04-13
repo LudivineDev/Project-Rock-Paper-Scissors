29da4e9adf27ebc295d4fee2b5cd82c9316afea3
LudivineDev <183041232+LudivineDev@users.noreply.github.com>
2025-04-13 13:04:51 -0700

    2nd commit

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getComputerChoice() {
-    console.log("Hello 2 World");
+    let choice;
+    let randomnumber = Math.random();
+    if (randomnumber<0.33) {
+        choice="Paper";
+    } else if (randomnumber<0.66) {
+        choice="Scissors"
+    } else {
+        choice="Rock"
+    }
+    console.log(choice);
+    console.log(randomnumber)
 }
 getComputerChoice();
