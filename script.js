8282e0b6bb8a41318bf99f6c320f335a578a0c59
LudivineDev <183041232+LudivineDev@users.noreply.github.com>
2025-04-13 14:51:37 -0700

    4th commit

--- script.js
+++ script.js
@@ -2,26 +2,48 @@ function getComputerChoice() {
     let choice;
     let randomnumber = Math.random();
     if (randomnumber<0.33) {
-        choice="Paper";
+        choice="paper";
     } else if (randomnumber<0.66) {
-        choice="Scissors"
+        choice="scissors"
     } else {
-        choice="Rock"
+        choice="rock"
     }
     console.log(choice);
     console.log(randomnumber)
+    return choice;
 }
-getComputerChoice();
 
 function getHumanChoice() {
-    let choiceh = prompt("Please Choose between Rock, Paper or Scissors");
+    let choiceh = prompt("Please Choose between rock, paper or scissors");
     console.log(choiceh);
+    return choiceh;
 }
-getHumanChoice();
 
 let humanScore = 0;
 console.log(humanScore);
 let computerScore = 0;
 console.log(computerScore);
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
 
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+console.log("Normalized: "+ humanChoice);
+console.log(computerChoice)
+if (humanChoice === computerChoice) {
+    Message = "That's a tie";
+} else if (
+    (humanChoice==="paper" && computerChoice==="rock")||
+    (humanChoice==="rock" && computerChoice==="scissors")||
+    (humanChoice==="scissors" && computerChoice==="paper")){
+
+    Message = ("You Win " + humanChoice + " Beats "+ computerChoice + " !");
+    } else {
+    Message = ("You Loose " + computerChoice + " Beats "+ humanChoice + " !");
+    }
+    console.log(Message);
+
+}
+
+playRound(humanChoice, computerChoice);
